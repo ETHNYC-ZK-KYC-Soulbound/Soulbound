c2c401477a095ec9a9ae47717258004389c6cc6d
Nicolás Acosta <nicacosta@frba.utn.edu.ar>
2022-06-26 08:42:17 -0400

    prettier

--- contracts/KYCVerification.sol
+++ contracts/KYCVerification.sol
@@ -8,8 +8,7 @@ contract KYCVerification is MainVerification {
         address[] memory _initialApprovers,
         address _admin,
         string memory _tokenURI
-    ) MainVerification(_initialApprovers, _admin, _tokenURI) {
-    }
+    ) MainVerification(_initialApprovers, _admin, _tokenURI) {}
 
     function addSubmission(address[] memory approvers, string[] memory cids)
         external
